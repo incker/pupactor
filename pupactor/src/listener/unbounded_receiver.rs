393418dc6b7505140f8cf48f44f852287371bc62
incker <iamlolylol@gmail.com>
2026-05-13 15:58:33 +0300

    Add ActorListener for mpsc::Receiver

--- pupactor/src/listener/unbounded_receiver.rs
+++ pupactor/src/listener/unbounded_receiver.rs
@@ -1,8 +1,15 @@
 use crate::{ActorListener, ActorMsg};
-use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::sync::mpsc;
 
 /// UnboundedReceiver
-impl<Msg, Command> ActorListener<Msg, Command> for UnboundedReceiver<ActorMsg<Msg, Command>> {
+impl<Msg, Command> ActorListener<Msg, Command> for mpsc::UnboundedReceiver<ActorMsg<Msg, Command>> {
+    #[inline(always)]
+    async fn next_msg(&mut self) -> Option<ActorMsg<Msg, Command>> {
+        self.recv().await
+    }
+}
+
+impl<Msg, Command> ActorListener<Msg, Command> for mpsc::Receiver<ActorMsg<Msg, Command>> {
     #[inline(always)]
     async fn next_msg(&mut self) -> Option<ActorMsg<Msg, Command>> {
         self.recv().await
